9b83fad37423c729b60d70fafcf30fbb0773f332
Max! <admin@cloudydev.net>
2023-09-19 22:37:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ module "scale-set" {
   scale_in_rule                   = "NewestVM"
   scale_in_force_deletion_enabled = true
   user_data_path                  = "${var.user_data_path}"
-  username                        = var.username
+  username                        = var.github_username
   hostname                        = var.hostname
   github_username                 = var.github_username
   vm_admin_username               = local.admin_identity
